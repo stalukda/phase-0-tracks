b8c4bd06d0feeab4f7c052c31ff271128bde075e
Sumaiya Talukdar <talukdar.sumaiya@gmail.com>
2016-09-04 21:31:58 -0700

    Solo challenge h/w 5.5

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,30 +1,37 @@
-# Swapping the first and last name.
-# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
-# and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. 
-# So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
-
-# puts "What is your first name?"
-# first_name = gets.chomp 
-
-# puts "What is your last name?"
-# last_name = gets.chomp 
-
+# Swap the first and last name by subbing them for each other 
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou' by using next 
+# and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet by usnig next
 
 def alias_builder(first_name, last_name)
-	puts "Hello #{first_name} #{last_name}"
+	# Swap the first and last name by subbing them for each other 
 	newname1 = first_name.sub(first_name, last_name)
 	newname2= last_name.sub(last_name, first_name)
-	# puts "Hello #{newname1} #{newname2}"
+	#declare swapped_name variable to store the swapped name
 	swapped_name = newname1 + newname2
-	# puts "Hello #{swapped_name}"
-
-# split the letters so we have an array
-	p name_arr = swapped_name.split("")
-# change the vowels to the next vowel
+	# split the letters in swapped_name so we have an array
+	name_arr = swapped_name.split("")
+	# change the vowels & consonants to the next in the series 
 	p name_arr.map!{ |letters| letters.next }
-# change the consonants to the next consonant 
-	
 end 
 
-alias_builder("Felicia" , "Torres")
+#Create an until loop to keep asking for names to be coded until the user types 'quit'
+
+first_name = " "
+last_name = " "
+
+until first_name == "quit" || last_name == "quit"
+
+	puts "What is your first name? Enter 'quit' to exit"
+	first_name = gets.chomp 
+
+	unless first_name == "quit"
+		puts "What is your last name?"
+		last_name = gets.chomp 
+		puts "Hello #{first_name} #{last_name}"
+		puts "Here is your alias for #{first_name}  #{last_name}:"
+		alias_builder(first_name, last_name)
+	end
+
+end 
+
 
