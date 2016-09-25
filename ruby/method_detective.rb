d846d48c484d604e5af8c76ae5f0d4a46a927ed3
Sumaiya Talukdar <talukdar.sumaiya@gmail.com>
2016-09-25 16:25:26 -0700

    5.1 complete

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -34,6 +34,7 @@ p "Elementary,    my   dear        Watson!".squeeze(" ")
 "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#its the ASCII value 
 
 "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
